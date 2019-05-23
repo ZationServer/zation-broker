6dc6bcab4e8a3a2e56d437d6dae281e07df0c1a8
Luca Scaringella <lucalstyle@gmail.com>
2019-05-23 20:09:22 +0200

    add new -e state server host option

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const argv = require('minimist')(process.argv.slice(2));
 const packageVersion = require('./package.json').version;
 
 const DEFAULT_PORT = 8888;
-const SCC_STATE_SERVER_HOST = process.env.STATE_SERVER_HOST || process.env.ssh || argv.zcsh ||
+const SCC_STATE_SERVER_HOST = process.env.STATE_SERVER_HOST || process.env.ssh || argv.zcsh || process.env.zcsh ||
     process.env.SCC_STATE_SERVER_HOST || process.env.ZATION_STATE_SERVER_HOST;
 const SCC_STATE_SERVER_PORT = Number(process.env.STATE_SERVER_PORT) || Number(process.env.SCC_STATE_SERVER_PORT) || Number(process.env.ZATION_STATE_SERVER_PORT) || 7777;
 const SCC_INSTANCE_IP = process.env.SCC_INSTANCE_IP || null;
 const SCC_INSTANCE_IP_FAMILY = process.env.SCC_INSTANCE_IP_FAMILY || 'IPv4';
 const SCC_AUTH_KEY = process.env.cak || process.env.CLUSTER_AUTH_KEY || process.env.SCC_AUTH_KEY || null;
 const RETRY_DELAY = Number(argv.r) || Number(process.env.SCC_BROKER_SERVER_RETRY_DELAY) || 2000;
-const SC_LOG = process.env.SC_LOG || false;
 const STATE_SERVER_CONNECT_TIMEOUT = Number(process.env.SCC_STATE_SERVER_CONNECT_TIMEOUT) || 3000;
 const STATE_SERVER_ACK_TIMEOUT = Number(process.env.SCC_STATE_SERVER_ACK_TIMEOUT) || 2000;
 const BROKER_SERVER_CONNECT_TIMEOUT = Number(process.env.SCC_BROKER_SERVER_CONNECT_TIMEOUT) || 10000;
