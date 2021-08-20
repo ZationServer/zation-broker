ba7728ee768bd053eadb4ba8163c85142deb4c59
Luca Scaringella <lucalstyle@gmail.com>
2021-08-20 17:34:13 +0200

    Use exported LogLevel enum.

--- src/run.ts
+++ src/run.ts
@@ -4,7 +4,7 @@ GitHub: LucaCode
 Copyright(c) Luca Scaringella
  */
 
-import {BrokerServer} from "ziron-broker";
+import {BrokerServer,LogLevel} from "ziron-broker";
 import {secrets} from "docker-secret";
 
 const variables = Object.assign({}, process.env, secrets);
@@ -18,5 +18,5 @@ new BrokerServer({
       : `${variables.SECRET || ""}@${variables.STATE || ""}`,
   port: parseInt(variables.PORT) || 8888,
   path: variables.PATH || "/",
-  logLevel: parseInt(variables.LOG_LEVEL) || 3,
+  logLevel: parseInt(variables.LOG_LEVEL) || LogLevel.Everything,
 }).joinAndListen();
